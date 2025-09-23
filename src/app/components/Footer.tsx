12084fc2b9a0e7c73b60d4ad0dd8764b1b91bfab
Denis <Nigazzz2000@gmail.com>
2025-09-23 14:38:53 +0700

    feat: Создал модуль для текстов | Добавил брейкпоинты & Цвета

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import t from "@/app/styles/modules/typography.module.css"
 
 export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <h1 className="uppercase text-black text-xl text-center">Footer</h1>
+        <h1 className={`${t.heading} text-center`}>Контакты</h1>
       </div>
     </footer>
   );
