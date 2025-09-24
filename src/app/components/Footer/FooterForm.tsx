0d8259b52954afbaeb3c16ae39604c58613adff9
Denis <Nigazzz2000@gmail.com>
2025-09-24 13:02:50 +0700

    feat: Добавил чекбокс в форму

--- src/app/components/Footer/FooterForm.tsx
+++ src/app/components/Footer/FooterForm.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import t from "@/app/styles/modules/typography.module.css";
+import FormButton from "./FormButton";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function FooterForm() {
   return (
@@ -23,9 +26,16 @@ export default function FooterForm() {
           placeholder="Ваш телефон"
           value="+7 (XXX) XXX-XX-XX"
         />
-        <button type="submit" className="bg-black text-white py-2 px-4 rounded">
-          Отправить
-        </button>
+        <div className="flex gap-[0.62rem] items-center">
+          <Checkbox className="h-[1rem] w-[1rem]" />
+          <p className={`${t["overline"]}`}>
+            Я согласен с{" "}
+            <Link className="underline" href="/privacy-policy">
+              политикой конфиденциальности
+            </Link>
+          </p>
+        </div>
+        <FormButton />
       </form>
     </div>
   );
---
+++ src/app/components/Footer/FormButton.tsx
@@ -0,0 +1,10 @@
+export default function FormButton() {
+  return (
+    <button
+      type="submit"
+      className="bg-sand text-white text-left rounded-[3.12rem] py-[0.93rem] pl-[1.87rem] w-full md:py-[1.25rem] md:px-[2.18rem]"
+    >
+      Отправить
+    </button>
+  );
+}
