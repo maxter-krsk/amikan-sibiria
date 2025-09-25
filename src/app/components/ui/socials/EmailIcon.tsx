bcf04616250937ddea9573a4e91083c855be16cb
Denis <Nigazzz2000@gmail.com>
2025-09-25 17:01:03 +0700

    feat: Добавил className в проп иконкам

--- src/app/components/ui/socials/EmailIcon.tsx
+++ src/app/components/ui/socials/EmailIcon.tsx
@@ -1,5 +1,13 @@
-export default function EmailIcon () {
+type EmailIconProps = {
+	className?: string;
+}
+
+export default function EmailIcon ({className}: EmailIconProps) {
 	return (
-		<img src="/icons/ui/socials-icons/mail.svg" width={24} height={24} alt="Почта" />
+		<img src="/icons/ui/socials-icons/mail.svg"
+		width={24}
+		height={24}
+		className={className}
+		alt="Почта" />
 	)
 }
--- src/app/components/ui/socials/PhoneIcon.tsx
+++ src/app/components/ui/socials/PhoneIcon.tsx
@@ -1,5 +1,15 @@
-export default function PhoneIcon() {
-	return (
-		<img src="/icons/ui/socials-icons/phone.svg" width={24} height={24} alt="Телефон" />
-	)
+type PhoneIconProps = {
+  className?: string;
+};
+
+export function PhoneIcon({className}: PhoneIconProps) {
+  return (
+    <img
+      src="/icons/ui/socials-icons/phone.svg"
+      width={24}
+      height={24}
+      alt="Телефон"
+	  className={className}
+    />
+  );
 }
