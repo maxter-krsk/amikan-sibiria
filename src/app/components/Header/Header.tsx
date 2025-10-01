6dd20e11757ea83590b2e372eb843ef69257e7e4
Kirill Khaylenko <alexfrol219@gmail.com>
2025-10-01 11:59:45 +0700

    feat: Завершил работу над структурой и стилизацией шапки || Добавил соц. сети в шапку

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,20 +1,57 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import BurgerMenu from "./Burger";
 import t from "@/app/styles/modules/typography.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const LIGHT_LOGO = "/icons/logos/logo-light.svg";
+  const DARK_LOGO = "/icons/logos/logo-dark.svg";
+
   return (
     <header>
       <div className="container">
         <div className="flex justify-between items-center py-[1.2rem]">
-          <Link href="/">
-            <Image
-              src="/icons/logos/logo-light.svg"
-              alt="Логотип Амикан"
-              width={64}
-              height={64}
-            ></Image>
+          <Link href="/" aria-label="На главную" className="block">
+            {mounted && (
+              <>  
+                <Image
+                  className="md:hidden"
+                  src={isOpen ? DARK_LOGO : LIGHT_LOGO}
+                  alt="Логотип Амикан (мобильный)"
+                  width={64}
+                  height={64}
+                  priority
+                />
+                <Image
+                  className="hidden md:block desk:hidden"
+                  src={LIGHT_LOGO}
+                  alt="Логотип Амикан (планшет)"
+                  width={64}
+                  height={64}
+                />
+                <Image
+                  className="hidden desk:block"
+                  src={isOpen ? DARK_LOGO : LIGHT_LOGO}
+                  alt="Логотип Амикан (десктоп)"
+                  width={64}
+                  height={64}
+                />
+              </>
+            )}
           </Link>
           <nav className="hidden desk:block px-[1.9rem] py-[1.3rem] bg-beige rounded-[6.2rem]">
             <ul className={`${t["body-lg"]} flex gap-[1.2rem] text-darkGreen`}>
@@ -48,7 +85,7 @@ export default function Header() {
             >
               Оставить заявку
             </Link>
-            <BurgerMenu />
+            <BurgerMenu isOpen={isOpen} toggleMenu={toggleMenu} />
           </div>
         </div>
       </div>
