4265ffc06aba4b37c0fe6b402976406c7b4589f1
Denis <Nigazzz2000@gmail.com>
2025-10-06 13:50:58 +0700

    feat: адаптировал блок по все устройства

--- src/app/components/sections/Tour/Tour.tsx
+++ src/app/components/sections/Tour/Tour.tsx
@@ -3,7 +3,7 @@ export default function Tour() {
   const tour = [
     {
       dayNumber: "1",
-      dayLabel: "День",
+      dayLabel: "день",
       title: "Прибытие в Абакан",
       activities: [
         "После приезда в Абакан вас встретят, отвезут на завтрак и пригласят на экскурсию в историко‑этнографический комплекс «Шушенское»",
@@ -13,7 +13,7 @@ export default function Tour() {
     },
     {
       dayNumber: "2",
-      dayLabel: "День",
+      dayLabel: "день",
       title: "Рыбалка на водохранилище",
       activities: [
         "На второй день после завтрака вас ожидает рыбалка в акватории Саяно‑Шушенского биосферного заповедника",
@@ -23,7 +23,7 @@ export default function Tour() {
     },
     {
       dayNumber: "3",
-      dayLabel: "День",
+      dayLabel: "день",
       title: "Переход на реку Кантегир",
       activities: [
         "Следующим утром стартует переход на аэролодках: примерно 60 км по водохранилищу и ещё 50 км вверх по горной реке Кантегир",
@@ -33,7 +33,7 @@ export default function Tour() {
     },
     {
       dayNumber: "4",
-      dayLabel: "День",
+      dayLabel: "день",
       title: "Сплав и возвращение",
       activities: [
         "Четвёртый день начинается с завтрака и неспешного возвращения по течению обратно к плавкомплексу",
@@ -42,7 +42,7 @@ export default function Tour() {
     },
     {
       dayNumber: "5",
-      dayLabel: "День",
+      dayLabel: "день",
       title: "Экскурсии и завершение тура",
       activities: [
         "Заключительный день тура начинается с выезда с плавкомплекса",
@@ -54,16 +54,18 @@ export default function Tour() {
   ];
 
   return (
-    <section className="relative py-10 bg-no-repeat bg-cover [background-position:-230px_top] md:bg-[url('/icons/ui/background-line.svg')] bg-none">
+    <section className="relative py-10 bg-no-repeat bg-cover [background-position:-270px_70px] md:bg-[url('/icons/ui/background-line.svg')] bg-none">
       <div
         aria-hidden
-        className="pointer-events-none absolute inset-y-0 left-5 md:left-1/2 md:-translate-x-1/2 w-[48px] md:w-[134px] desk:w-[259px] bg-darkGreen z-0"
+        className="pointer-events-none absolute inset-y-0 left-5 md:left-1/2 md:-translate-x-1/2 w-[48px] md:w-[8.37rem] desk:w-[16.18rem] bg-darkGreen z-0"
       />
       <div className="container relative z-10">
         <h1
-          className={`${t.heading} text-right md:text-left font-bold uppercase text-darkGreen md:w-[50%]`}
+          className={`${t.heading} mb-[1.87rem] md:mb-0 text-right md:text-left font-bold uppercase text-darkGreen`}
         >
-          Программа тура
+          Программа
+          <br />
+          тура
         </h1>
         {tour.map((touritem, i) => {
           const isEven = (i + 1) % 2 === 0;
@@ -71,7 +73,7 @@ export default function Tour() {
             <div
               key={i}
               className={`
-                max-w-[52%] flex gap-[2.5rem] md:gap-[1.87rem] desk:gap-[3.12rem]
+                md:max-w-[50%] flex
                 ${
                   isEven
                     ? "md:mr-auto md:flex-row-reverse md:text-right"
@@ -79,30 +81,46 @@ export default function Tour() {
                 }
               `}
             >
-              <div className={`
-                text-beige font-bold text-right
+              <div
+                className={`
+                text-beige font-bold
+                ${isEven ? "md:text-left" : "md:text-right"}
+                `}
+              >
+                <div
+                  className={`
+                  ${
+                    isEven
+                      ? "ml-[0.3rem] md:relative md:left-[1.68rem] desk:left-0"
+                      : "ml-[0.3rem] md:relative md:right-[1.68rem] desk:right-0"
+                  }
+                  `}
+                >
+                  <p className="text-[2.5rem] md:text-[3.75rem] desk:text-[6.25rem] leading-none">
+                    {touritem.dayNumber}
+                  </p>
+                  <p className="text-[0.62rem] font-semibold md:text-[1.87rem] desk:text-[2.5rem]">
+                    {touritem.dayLabel}
+                  </p>
+                </div>
+              </div>
+              <div
+                className={` flex flex-col gap-[0.62rem] md:gap-[1.87rem]
                 ${
                   isEven
-                    ? "md:text-left"
-                    : "md:text-right"
+                    ? "ml-[1.86rem] md:ml-0 desk:mr-[3.12rem]"
+                    : "ml-[1.86rem] md:ml-0 desk:ml-[3.12rem]"
                 }
-                `}>
-                <p className="text-[2.5rem] md:text-[3.75rem] desk:text-[6.25rem]">
-                  {touritem.dayNumber}
-                </p>
-                <p className="text-[0.62rem] md:text-[1.87rem] desk:text-[2.5rem]">
-                  {touritem.dayLabel}
-                </p>
-              </div>
-              <div className="flex flex-col gap-[1.87rem]">
-                <p className="text-[1.12rem] md:text-[1.5rem] desk:text-[1.87rem] font-medium text-darkGreen">
+                `}
+              >
+                <p className="backdrop-blur-3xl text-[1.12rem] md:text-[1.5rem] desk:text-[1.87rem] font-medium text-darkGreen">
                   {touritem.title}
                 </p>
                 <div className="flex flex-col gap-[0.93rem] md:gap-[1.25rem] desk:gap-[1.87rem] text-darkGreen font-normal text-[0.62rem] md:text-[0.87rem] desk:text-[1.12rem]">
-                  <p>{touritem.activities[0]}</p>
-                  <p>{touritem.activities[1]}</p>
-                  <p>{touritem.activities[2]}</p>
-                  <p>{touritem.activities[3]}</p>
+                  <p className="backdrop-blur-3xl">{touritem.activities[0]}</p>
+                  <p className="backdrop-blur-3xl">{touritem.activities[1]}</p>
+                  <p className="backdrop-blur-3xl">{touritem.activities[2]}</p>
+                  <p className="backdrop-blur-3xl">{touritem.activities[3]}</p>
                 </div>
               </div>
             </div>
