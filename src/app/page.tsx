5818e7ef191f6805c96f67fac9c72bd5f63205b5
Denis <Nigazzz2000@gmail.com>
2025-10-06 22:37:58 +0700

    fix: Импортировал блок Services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Contacts from "@/app/components/sections/Contacts/Contacts";
 import Feedbacks from "@/app/components/sections/Feedbacks/Feedbacks";
 import Gallery from "@/app/components/sections/Gallery/Gallery";
 import Hero from "./components/sections/Hero/Hero";
+import Services from "./components/sections/Services/Services";
 import Tour from "./components/sections/Tour/Tour";
 
 export default function Home() {
