57b19e5d03f59058a9502fb96cc8e8bf9a629ccb
Kirill Khaylenko <alexfrol219@gmail.com>
2025-10-07 17:23:59 +0700

    feat: Адаптировал вторую часть hero-блока || Перевёл всё в единицы rem || Убрал неиспользуемые импорты || Исправил сетку для блоков

--- src/app/components/sections/Hero/Hero.tsx
+++ src/app/components/sections/Hero/Hero.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
-import t from "@/app/styles/modules/typography.module.css";
 import { Button } from "@/app/components/ui/Button";
 import ProgramPopup from "../../ui/popups/ProgramPopup";
 import bg from "@/app/styles/modules/herobg.module.css";
@@ -16,20 +15,24 @@ export default function Hero() {
 
   return (
     <section
-      className={`${bg.heroBg} mx-0 desk:mt-[1.25rem] desk:mx-[1.25rem]  mb-[150px] pb-[60px] pt-[145px] relative`}
+      className={`${bg.heroBg} mx-0 desk:mt-[1.25rem] desk:mx-[1.25rem] mb-[9.38rem] pb-[3.75rem] pt-[9.06rem] relative`}
       aria-labelledby="hero-title"
     >
       <div className="container">
         <div className="flex gap-[0.62rem] desk:flex-row desk:items-end mb-[1.25rem] desk:mb-[1.87rem] md:flex-col flex-col">
-          <h1 className="font-bold desk:text-[70px] md:text-[40px] text-[28px] text-center md:text-left leading-[1.2] uppercase text-[#FFFFFF]">
-            Сибирь, которую<br/>стоит увидеть
+          <h1 className="font-bold desk:text-[4.38rem] md:text-[2.5rem] text-[1.75rem] text-center md:text-left leading-[1.2] uppercase text-[#FFFFFF]">
+            Сибирь, которую
+            <br />
+            стоит увидеть
           </h1>
-          <p className="font-medium desk:text-[22px] md:text-[18px] text-[12px] text-center md:text-left text-[#FFFFFF]">
-            Отдых и рыбалка на Саяно-Шушенском<br/>водохранилище и Саянской реке Кантегир
+          <p className="font-medium desk:text-[1.38rem] md:text-[1.12rem] text-[0.75rem] text-center md:text-left text-[#FFFFFF]">
+            Отдых и рыбалка на Саяно-Шушенском
+            <br />
+            водохранилище и Саянской реке Кантегир
           </p>
         </div>
         <div
-          className="flex flex-col gap-[0.62rem] md:gap-[1.25rem] mb-[3.12rem] items-center md:items-start"
+          className="flex flex-col gap-[0.62rem] md:gap-[1.25rem] mb-[6.87rem] md:mb-[5.62rem] desk:mb-[3.12rem] items-center md:items-start"
           aria-describedby="cta-hint"
         >
           <Button
@@ -40,53 +43,58 @@ export default function Hero() {
             Получить программу тура
           </Button>
           <ProgramPopup open={open} onOpenChange={() => setOpen(!open)} />
-          <p id="cta-hint" className="font-normal text-[0.75rem] md:text-[0.87rem] desk:text[1rem] text-center md:text-left text-[#FFFFFF]">
-            Оставьте контакты — мы пришлем<br className="md:hidden"/>подробное описание
+          <p
+            id="cta-hint"
+            className="font-normal text-[0.75rem] md:text-[0.87rem] desk:text-[1rem] text-center md:text-left text-[#FFFFFF]"
+          >
+            Оставьте контакты — мы пришлем
+            <br className="md:hidden" />
+            подробное описание
           </p>
         </div>
-        <ul className="flex gap-[1.25rem]">
-          <li className="p-[20px] flex gap-[1.25rem] bg-beige rounded-[30px] max-w-[710px]">
-            <Image
-              className="rounded-[20px]"
-              src="/images/hero/hero-scenery.jpg"
-              alt="Панорамный вид на СШГЭС"
-              width={305}
-              height={196}
-            />
-            <div className="flex flex-col gap-[48px] text-darkGreen">
-              <p className="leading-5">
-                <strong className="font-semibold text-[16px]">
+        <ul className="flex flex-col items-center md:flex-row md:items-stretch md:flex-nowrap gap-[0.62rem] md:gap-[1.25rem]">
+          <li className="w-[18.75rem] md:w-[22.12rem] desk:w-[44.38rem] p-[0.93rem] md:p-[1.25rem] bg-beige rounded-[0.62rem] md:rounded-[1.25rem] desk:rounded-[1.87rem]">
+            <div className="flex gap-[1.25rem]">
+              <Image
+                className="w-[19.06rem] h-[12.25rem] object-cover rounded-[1.25rem] hidden desk:block shrink-0"
+                src="/images/hero/hero-scenery.jpg"
+                alt="Панорамный вид на СШГЭС"
+                width={305}
+                height={196}
+              />
+              <div className="flex flex-col gap-[0.62rem] md:gap-[2.5rem] desk:justify-between text-darkGreen">
+                <p className="font-semibold text-[0.75rem] md:text-[1rem] leading-[1.25] text-center md:text-start [text-wrap:balance]">
                   Премиальный тур для тех, кто хочет насладиться природой и
                   отдохнуть в комфортных условиях
-                </strong>
-              </p>
-              <p className="font-medium text-[18px] leading-6">
-                Панорамные виды, тёплая база, авторская кухня. Заезд на
-                музей-усадьбу В.И. Ленина и экскурсия по окрестностям
-              </p>
+                </p>
+                <p className="font-medium text-center md:text-start text-[0.62rem] md:text-[0.87rem] desk:text-[1.12rem] leading-[1.2] whitespace-normal break-words [hyphens:auto]">
+                  Панорамные виды, тёплая база, авторская кухня. Заезд на
+                  музей-усадьбу В.И. Ленина и экскурсия по окрестностям
+                </p>
+              </div>
             </div>
           </li>
 
-          <li className="p-[20px] flex flex-row-reverse gap-[1.25rem] bg-beige rounded-[30px] max-w-[670px]">
-            <Image
-              className="rounded-[20px]"
-              src="/images/hero/hero-man.jpg"
-              alt="Панорамный вид на СШГЭС"
-              width={305}
-              height={196}
-            />
-            <div className="flex flex-col justify-between gap-[112px] text-darkGreen">
-              <p className="leading-5 max-w-[340px]">
-                <strong className="font-semibold text-[16px]">
+          <li className="w-[18.75rem] md:w-[22.12rem] desk:w-[44.38rem] p-[0.93rem] md:p-[1.25rem] bg-beige rounded-[0.62rem] md:rounded-[1.25rem] desk:rounded-[1.87rem]">
+            <div className="flex gap-[1.25rem] desk:flex-row-reverse h-full">
+              <Image
+                className="w-[19.06rem] h-[12.25rem] object-cover rounded-[1.25rem] hidden desk:block shrink-0"
+                src="/images/hero/hero-man.jpg"
+                alt="Панорамный вид на СШГЭС"
+                width={305}
+                height={196}
+              />
+              <div className="flex flex-col justify-between text-darkGreen">
+                <p className="font-semibold text-[0.75rem] md:text-[1rem] leading-[1.25] text-center md:text-start [text-wrap:balance]">
                   Приезжайте семьей, в компании друзей или коллег — будет
                   интересно и уютно
-                </strong>
-              </p>
-              <p className="uppercase font-semibold text-[20px]">
-                <Link href="/" aria-label="Узнать больше о туре">
-                  Узнать больше
-                </Link>
-              </p>
+                </p>
+                <p className="hidden md:block uppercase font-semibold md:text-[1.12rem] desk:text-[1.25rem]">
+                  <Link href="/" aria-label="Узнать больше о туре">
+                    Узнать больше
+                  </Link>
+                </p>
+              </div>
             </div>
           </li>
         </ul>
