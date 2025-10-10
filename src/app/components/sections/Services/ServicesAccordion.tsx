bf468d6c1d9ae260e2e458d783488ad3775814b8
exrsxe <nigazzz2000@gmail.com>
2025-10-10 15:03:48 +0700

    feat: Адаптировал отступы, размеры согласно утилитарным классам

--- src/app/components/sections/Services/ServicesAccordion.tsx
+++ src/app/components/sections/Services/ServicesAccordion.tsx
@@ -14,18 +14,18 @@ export default function ServicesAccordion() {
     <div className="flex flex-col items-center md:items-start">
       <button
         onClick={handleOnClick}
-        className="flex gap-[1.56rem] md:gap-[1.87rem] desk:gap-[3.12rem] items-center pb-[0.93rem] md:pb-[1.25rem] cursor-pointer"
+        className="flex gap-25 md:gap-30 desk:gap-50 items-center pb-15 md:pb-20 cursor-pointer"
       >
-        <span className={`${t.display} uppercase  font-bold text-beige`}>
+        <span className={`${t.display} uppercase font-bold text-beige`}>
           Что взять с собой
         </span>
-        <span className="text-[1.62rem] md:text-[1.87rem] font-semibold text-white bg-sand rounded-full w-[3.12rem] h-[3.12rem] desk:w-[4rem] desk:h-[4rem] flex items-center justify-center">
+        <span className="text-26 md:text-30 font-semibold text-white bg-sand rounded-full w-[3.12rem] h-[3.12rem] desk:w-[4rem] desk:h-[4rem] flex items-center justify-center">
           +
         </span>
       </button>
       {isOpen && (
-        <div className="w-full border border-sand rounded-[1.87rem] mb-[1.25rem]">
-          <p className="text-beige text-[0.75rem] md:-text-[1rem] desk:text-[1.12rem] p-[0.93rem] md:p-[1.25rem] desk:p-[1.87rem]">
+        <div className="w-full border border-sand rounded-[1.87rem] mb-20">
+          <p className="text-beige text-12 md:-text-16 desk:text-18 p-15 md:p-20 desk:p-30">
             Рюкзак, солнцезащитные очки, личная аптечка, средства личной
             гигиены, дождевик, лёгкий пуховик, головной убор, термобельё,
             флисовая одежда, футболки, рабочие и тёплые перчатки, носки, бафф,
