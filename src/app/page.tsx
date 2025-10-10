80b71d4610d830a31453a097ddf87c49e47fb360
Kirill Khaylenko <alexfrol219@gmail.com>
2025-10-10 14:05:50 +0700

    feat: Создал компоненты для раздела преимуществ и тестово оформил блок

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import Gallery from "@/app/components/sections/Gallery/Gallery";
 import Hero from "./components/sections/Hero/Hero";
 import Services from "./components/sections/Services/Services";
 import Tour from "./components/sections/Tour/Tour";
+import Features from "./components/sections/Advantages/Features";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Features />
       <Tour />
       <Services />
       <Feedbacks />
