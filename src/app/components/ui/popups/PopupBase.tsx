871257662b11ded85110262adb53901036970332
Kirill Khaylenko <alexfrol219@gmail.com>
2025-10-16 16:13:55 +0700

    feat: Изменил размеры, сетку, отступы, расположение элементов

--- src/app/components/ui/popups/PopupBase.tsx
+++ src/app/components/ui/popups/PopupBase.tsx
@@ -28,15 +28,15 @@ export default function PopupBase({
       <DialogContent
         showCloseButton={false}
         className="
-          bg-darkGreen text-beige border-none rounded-[1.25rem] desk:rounded-[3.12rem]
-          p-[0.62rem] md:p-[1.87rem]
+          bg-darkGreen text-beige border-none !rounded-20 desk:!rounded-50
+          p-20 md:p-30
           max-w-[calc(100vw-0.62rem*2)] md:max-w-[38.75rem] desk:max-w-[64.37rem]
-          gap-[1.25rem] md:gap-[1.87rem] desk:gap-[2.5rem]
+          gap-20 md:gap-30 desk:gap-40
         "
       >
         <DialogHeader>
           <div className="flex flex-col-reverse md:flex md:flex-row justify-between">
-            <DialogTitle className="text-[1.5rem] md:text-[1.87rem] desk:text-[2.5rem] pr-[1.25rem] uppercase font-bold text-left">
+            <DialogTitle className="text-24 md:text-30 desk:text-40 pr-20 uppercase font-bold text-left">
               {title}
             </DialogTitle>
             {closeIt}
