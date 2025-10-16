d25e8650a7cba0eee7f27089c4c9de48eaed7f86
exrsxe <nigazzz2000@gmail.com>
2025-10-16 13:20:07 +0700

    fix: исправил цвет чекбоса активного на темной теме

--- src/app/components/sections/Contacts/ContactsForm.tsx
+++ src/app/components/sections/Contacts/ContactsForm.tsx
@@ -40,8 +40,8 @@ export default function ContactsForm() {
       <div className="flex flex-col gap-10 md:gap-20 desk:flex-row justify-between">
         <div className="flex gap-10 items-top w-full">
           <Checkbox
-            className="h-[1rem] w-[1rem] border border-darkGreen rounded-[0.2rem] data-[state=checked]:bg-transparent
-          data-[state=checked]:text-darkGreen data-[state=checked]:border-darkGreen"
+            className="h-[1rem] w-[1rem] border border-darkGreen rounded-[0.2rem] data-[state=checked]:bg-transparent dark:data-[state=checked]:bg-transparent
+          data-[state=checked]:text-darkGreen data-[state=checked]:border-darkGreen color-scheme-light"
             required
           />
           <p className={`${t["overline"]} w-full`}>
